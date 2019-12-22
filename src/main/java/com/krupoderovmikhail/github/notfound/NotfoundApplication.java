11eaf0d8e3b974819a5240af38b33b9243d58bbb
KrupoderovMikhail <mishka.crash@gmail.com>
2019-12-22 21:31:04 +0500

    Initial commit.

---
+++ src/main/java/com/krupoderovmikhail/github/notfound/NotfoundApplication.java
@@ -0,0 +1,13 @@
+package com.krupoderovmikhail.github.notfound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotfoundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotfoundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/krupoderovmikhail/github/notfound/NotfoundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krupoderovmikhail.github.notfound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotfoundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
